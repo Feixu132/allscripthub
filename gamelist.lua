58fd6addffd9ae48cd3d8dd6c5738cfa944af833
Feixu132 <110287568+Feixu132@users.noreply.github.com>
2024-12-08 08:32:58 +0800

    Create gamelist.lua

---
+++ gamelist.lua
@@ -0,0 +1,6 @@
+getgenv().Games = {
+  [2753915549] = "https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua", -- Blox Fruit Sea 1
+  [4442272183] = "https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua", -- Blox Fruit Sea 2
+  [7449423635] = "https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua", -- Blox Fruit Sea 3
+  [16732694052] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Fisch.lua", -- Fisch
+}
